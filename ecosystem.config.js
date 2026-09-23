0f30586d19fd9b6ddcf8050a0421274b62867922
Bambang Friadi <bambang.friadi@gmail.com>
2026-09-23 13:47:25 +0700

    Fix: Hardcode dotenv path in server.js to __dirname and revert PM2 cwd to avoid script not found errors

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'masjid-backend',
-      script: 'server.js',
-      cwd: './backend',
+      script: './backend/server.js',
       instances: 1,
       autorestart: true,
       watch: false,
