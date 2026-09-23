24a565e4b9089a518eec287966bec758fbbacea5
Bambang Friadi <bambang.friadi@gmail.com>
2026-09-23 14:38:05 +0700

    Fix: Rename PWA icons to forcibly bypass Android aggressive caching

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,13 +23,13 @@ export default defineConfig({
         scope: '/masjid/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'pwa-maskable-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-maskable-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
